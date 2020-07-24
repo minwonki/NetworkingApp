1a9208132cd5ca8984661ce92ee2aa8babca089f
minwonki <wkimin@gmail.com>
2020-07-24 11:17:28 +0900

    Initial Commit

---
+++ NetworkingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkingApp
+//
+//  Created by WONKI MIN on 2020/07/24.
+//  Copyright © 2020 WONKI MIN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
